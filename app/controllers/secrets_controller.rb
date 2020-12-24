d3ed05d5d302869d518b5b4e704f1dbd00b98859
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-23 16:20:59 -0800

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,12 @@
+class SecretsController < ApplicationController
+    before_action :logged_in
+    def show
+        
+    end
+
+    def logged_in 
+        if !session[:name]
+            redirect_to new_session_path
+        end
+    end
+end
