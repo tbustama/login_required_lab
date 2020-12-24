2adf87e91a07654a517b32adf4ac017efa59c6ca
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-23 16:55:41 -0800

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:name] = params[:name]
+    session[:name] = params[:user][:name]
     if session[:name] == nil || session[:name] == ''
       redirect_to new_session_path
     else
-      redirect_to '/'
+      redirect_to user_path
     end
   end
+
+  def show
+
+  end
+
+  
 
   def destroy
     session.delete :name
   end
+
+  
 end
